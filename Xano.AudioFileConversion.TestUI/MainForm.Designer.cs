2e703c4da96c8a3a99e260e8792ec4419ab5ae9e
Greg Cummines <greg_cummines@starkey.com>
2018-10-25 07:08:44 -0500

    Get folder picker working in UI.

--- Xano.AudioFileConversion.TestUI/MainForm.Designer.cs
+++ Xano.AudioFileConversion.TestUI/MainForm.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInputDirectory = new System.Windows.Forms.TextBox();
+            this.txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.btnInputDirectory = new System.Windows.Forms.Button();
+            this.btnOutputDirectory = new System.Windows.Forms.Button();
+            this.btnStartConversion = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // txtInputDirectory
+            // 
+            this.txtInputDirectory.Location = new System.Drawing.Point(22, 27);
+            this.txtInputDirectory.Name = "txtInputDirectory";
+            this.txtInputDirectory.Size = new System.Drawing.Size(441, 20);
+            this.txtInputDirectory.TabIndex = 0;
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.Location = new System.Drawing.Point(22, 64);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.Size = new System.Drawing.Size(441, 20);
+            this.txtOutputDirectory.TabIndex = 1;
+            // 
+            // btnInputDirectory
+            // 
+            this.btnInputDirectory.Location = new System.Drawing.Point(469, 27);
+            this.btnInputDirectory.Name = "btnInputDirectory";
+            this.btnInputDirectory.Size = new System.Drawing.Size(96, 23);
+            this.btnInputDirectory.TabIndex = 2;
+            this.btnInputDirectory.Text = "Input Directory";
+            this.btnInputDirectory.UseVisualStyleBackColor = true;
+            this.btnInputDirectory.Click += new System.EventHandler(this.btnInputDirectory_Click);
+            // 
+            // btnOutputDirectory
+            // 
+            this.btnOutputDirectory.Location = new System.Drawing.Point(469, 64);
+            this.btnOutputDirectory.Name = "btnOutputDirectory";
+            this.btnOutputDirectory.Size = new System.Drawing.Size(96, 23);
+            this.btnOutputDirectory.TabIndex = 3;
+            this.btnOutputDirectory.Text = "Output Directory";
+            this.btnOutputDirectory.UseVisualStyleBackColor = true;
+            this.btnOutputDirectory.Click += new System.EventHandler(this.btnOutputDirectory_Click);
+            // 
+            // btnStartConversion
+            // 
+            this.btnStartConversion.BackColor = System.Drawing.Color.Lime;
+            this.btnStartConversion.Location = new System.Drawing.Point(213, 106);
+            this.btnStartConversion.Name = "btnStartConversion";
+            this.btnStartConversion.Size = new System.Drawing.Size(117, 35);
+            this.btnStartConversion.TabIndex = 4;
+            this.btnStartConversion.Text = "Start Conversion";
+            this.btnStartConversion.UseVisualStyleBackColor = false;
+            this.btnStartConversion.Click += new System.EventHandler(this.btnStartConversion_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(582, 271);
+            this.Controls.Add(this.btnStartConversion);
+            this.Controls.Add(this.btnOutputDirectory);
+            this.Controls.Add(this.btnInputDirectory);
+            this.Controls.Add(this.txtOutputDirectory);
+            this.Controls.Add(this.txtInputDirectory);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
             this.Text = "Audio File Conversion";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInputDirectory;
+        private System.Windows.Forms.TextBox txtOutputDirectory;
+        private System.Windows.Forms.Button btnInputDirectory;
+        private System.Windows.Forms.Button btnOutputDirectory;
+        private System.Windows.Forms.Button btnStartConversion;
     }
 }
 
--- Xano.AudioFileConversion.TestUI/MainForm.cs
+++ Xano.AudioFileConversion.TestUI/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,53 @@ namespace Xano.AudioFileConversion.TestUI
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnStartConversion_Click(object sender, EventArgs e)
+        {
+            if (txtInputDirectory.Text.Length == 0 || txtOutputDirectory.Text.Length == 0)
+            {
+                MessageBox.Show("Must pick both input and output directories before starting the conversion", "Audio File Conversion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Directory.Exists(txtInputDirectory.Text))
+            {
+                MessageBox.Show("Input directory does not exist", "Audio File Conversion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Directory.Exists(txtOutputDirectory.Text))
+            {
+                MessageBox.Show("Output directory does not exist", "Audio File Conversion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnInputDirectory_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtInputDirectory.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnOutputDirectory_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtOutputDirectory.Text = fbd.SelectedPath;
+                }
+            }
         }
     }
 }
